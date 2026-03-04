dec83204252fbedcf079b3340efb92e7a2c7ad71
Kanjo-Elkamira-Ndi <kanjoel39@gmail.com>
2026-03-04 11:44:20 +0100

    Kanjo Elkamira Ndi - Added calculator Module

---
+++ Lab03/src/calculator.js
@@ -0,0 +1,8 @@
+// Matricule: [YOUR_MATRICULE]
+
+function add(a,b){ return a+b; }
+function subtract(a,b){ return a-b; }
+function multiply(a,b){ return a*b; }
+function divide(a,b){ return b!==0 ? a/b : "Cannot divide by zero"; }
+
+module.exports = { add, subtract, multiply, divide };
